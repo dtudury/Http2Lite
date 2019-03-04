5b43538a1c243b8ce4a6209ab4b1bda0736e55cb
David Tudury <david.tudury@gmail.com>
2019-03-03 22:58:21 -0800

    wip

--- test/HpackContexts.test.js
+++ test/HpackContexts.test.js
@@ -1,15 +1,25 @@
 /* globals describe it */
 const { expect } = require('chai')
 const HpackEncodingContext = require('../lib/utils/HpackEncodingContext')
+const HpackDecodingContext = require('../lib/utils/HpackDecodingContext')
 
 describe.only('HpackEncodingContext', () => {
   describe('encode', () => {
     it('encodes headers', () => {
       let hec = new HpackEncodingContext()
-      hec.encode([{ name: ':path', value: '/index.html' }])
-      hec.encode([{ name: ':path', value: '/index.htm' }])
-      hec.encode([{ name: ':path', value: '/' }])
-      hec.encode([{ name: ':pat', value: '/' }])
+      let encoding = hec.encode([
+        { name: ':path', value: '/index.html' },
+        { name: ':path', value: '/index.htm' },
+        { name: ':path', value: '/' },
+        { name: ':pat', value: '/' },
+        { name: ':pat', value: '/' },
+        { name: ':pat', value: '/' },
+        { name: ':pat', value: '/' }
+      ])
+      console.log(encoding)
+      let hdc = new HpackDecodingContext()
+      let decoding = hdc.decode(encoding)
+      console.log(decoding)
       // expect(decodeInteger(new Uint8Array([0x0a]), 3).value).to.equal(10)
     })
   })
