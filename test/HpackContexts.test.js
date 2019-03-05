9e314830702d1accb4f84b1af7bb988d681a8248
David Tudury <david.tudury@gmail.com>
2019-03-05 11:48:04 -0800

    wip

--- test/HpackContexts.test.js
+++ test/HpackContexts.test.js
@@ -1,7 +1,11 @@
 /* globals describe it */
 const { expect } = require('chai')
 const { HpackEncodingContext, HpackDecodingContext } = require('../lib/utils/HpackContexts')
+const { SIZE_UPDATE, NOT_INDEXED, NEVER_INDEXED } = require('../lib/constants')
 
+function _toNameValue (headers) {
+  return headers.map(header => ({ name: headers.name, value: headers.value }))
+}
 describe('HpackContext', () => {
   describe('encode/decode', () => {
     it('encodes and decodes headers', () => {
@@ -9,20 +13,21 @@ describe('HpackContext', () => {
         { name: ':path', value: '/index.html' },
         { name: ':path', value: '/index.htm' },
         { name: ':path', value: '/' },
+        { name: ':pat', value: '/', kind: NOT_INDEXED },
+        { name: ':pat', value: '/', kind: NEVER_INDEXED },
         { name: ':pat', value: '/' },
         { name: ':pat', value: '/' },
+        { maxSize: 0, kind: SIZE_UPDATE },
         { name: ':pat', value: '/' },
         { name: ':pat', value: '/' }
       ]
-      let hec = new HpackEncodingContext()
+      let hec = new HpackEncodingContext(100)
       let encoding = hec.encode(headers)
       let hdc = new HpackDecodingContext()
+      hdc.maximumTableSize = 100
       let decoding = hdc.decode(encoding)
-      expect(decoding).to.deep.equal(headers)
-    })
-  })
-  describe('_evict', () => {
-    it('shrinks table size appropriately', () => {
+      expect(_toNameValue(decoding)).to.deep.equal(_toNameValue(headers))
+      expect(hdc.maximumTableSize).to.equal(0)
     })
   })
 })
